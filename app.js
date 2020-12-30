e84860c30af6ee80568655bf534ba0a40938e297
John <jonathan.mourier@easiware.fr>
2020-12-30 12:44:37 +0100

    ajout require

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const test = require('./models/testmodels.js')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
---
+++ models/testmodels.js
@@ -0,0 +1,6 @@
+module.exports = class testmodels {
+
+    async getPoolRewardHisto() {
+      return "plop";
+    }
+}
